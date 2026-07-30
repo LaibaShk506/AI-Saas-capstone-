4315d61273f4d51120a04c6b060fa1863d87de8b
LaibaShk506 <salmansaeedabdullah@gmail.com>
2026-07-30 17:52:29 +0500

    Create main.py

---
+++ Backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model.analyzer import analyze_resume_text
+
+app = FastAPI(title="AI Resume & Content Analyzer API")
+
+class TextPayload(BaseModel):
+    text: str
+
+@app.get("/")
+def home():
+    return {"status": "API is running!"}
+
+@app.post("/analyze")
+def analyze(data: TextPayload):
+    analysis_result = analyze_resume_text(data.text)
+    return {"status": "success", "data": analysis_result}
